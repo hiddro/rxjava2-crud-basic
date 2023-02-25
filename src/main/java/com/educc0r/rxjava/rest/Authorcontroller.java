594c35aed9ada7bd179f7b90f2267fc98a6c9ce8
@EduCCor <edd.ckalb@gmail.com>
2023-02-25 17:29:38 -0500

    modificado: response entity - controlador

--- src/main/java/com/educc0r/rxjava/rest/Authorcontroller.java
+++ src/main/java/com/educc0r/rxjava/rest/Authorcontroller.java
@@ -6,7 +6,9 @@ import io.reactivex.Maybe;
 import io.reactivex.Single;
 import io.reactivex.schedulers.Schedulers;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
@@ -19,49 +21,57 @@ public class Authorcontroller {
     @Autowired
     private AuthorService authorService;
 
+    /*METODO GET BY ID*/
     @GetMapping("/{id}")
     public Maybe<ResponseEntity<Author>> getAuthor(@PathVariable Integer id) {
         return authorService.findAuthor(id)
                 .subscribeOn(Schedulers.io())
                 .map(s -> ResponseEntity
-                        .created(URI.create("/api/authors/" + s.getIdAuthor()))
-                        .body(Author.builder().idAuthor(s.getIdAuthor()).name(s.getName()).build()));
+                        .ok()
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .body(s));
     }
 
+    /*METODO GET ALL*/
     @GetMapping()
     public Single<ResponseEntity<List<Author>>> getAuthors(){
         return authorService.findAuthors()
                 .subscribeOn(Schedulers.io())
                 .map(s -> ResponseEntity
-                        .created(URI.create("/api/authors/"))
+                        .ok()
+                        .contentType(MediaType.APPLICATION_JSON)
                         .body(s));
     }
 
+    /*METODO POST*/
     @PostMapping()
     public Single<ResponseEntity<Author>> addAuthor(@RequestBody Author author) {
         return authorService.addAuthor(author)
                 .subscribeOn(Schedulers.io())
                 .map(s -> ResponseEntity
                         .created(URI.create("/api/authors/" + s.getIdAuthor()))
-                        .body(Author.builder().idAuthor(s.getIdAuthor()).name(s.getName()).build()));
+                        .body(s));
     }
 
+    /*METODO PUT*/
     @PutMapping("/{id}")
     public Single<ResponseEntity<Author>> modAuthor(@PathVariable Integer id, @RequestBody Author author){
         return authorService.updateAuthor(id, author)
                 .subscribeOn(Schedulers.io())
                 .map(s -> ResponseEntity
-                        .created(URI.create("/api/authors/" + s.getIdAuthor()))
-                        .body(Author.builder().idAuthor(s.getIdAuthor()).name(s.getName()).build()));
+                        .ok()
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .body(s));
     }
 
+    /*METODO DELETE*/
     @DeleteMapping("/{id}")
     public Maybe<ResponseEntity<Author>> delAuthor(@PathVariable Integer id){
         return authorService.deleteAuthor(id)
                 .subscribeOn(Schedulers.io())
                 .map(s -> ResponseEntity
-                        .created(URI.create("/api/authors/" + s.getIdAuthor()))
-                        .body(Author.builder().idAuthor(s.getIdAuthor()).name(s.getName()).build()));
+                        .ok()
+                        .body(s));
     }
 
 }
